4a4f98d0fefb6e5bfb34accbe1bd43ec5763a801
Luis Antonio Mendoza <luismendoza.dev@gmail.com>
2019-03-02 11:55:38 -0600

    Headers added to response

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*'])
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.append('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
+
 app.use('/api/v1/foods', foodsRouter);
 
 // error handler
